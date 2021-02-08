539351a0bef3273644569b703abaceb2aa4665e5
Filip <superpou1filip@gmail.com>
2021-02-08 17:19:27 +0100

    now it does not add _backup to the original file

--- builder.py
+++ builder.py
@@ -4,7 +4,7 @@ import PySimpleGUI as sg
 import os
 import subprocess
 
-branchselect = [[sg.Text("Paste github link to sm64pc repo and branch")],[sg.In(),sg.In(size=(7, 1))],[sg.Text("And type the name of repo folder (cannot contain spaces and special characters)")],[sg.In()],[sg.Text('modelpack folder (optional)')],[sg.In(),sg.FolderBrowse()],[sg.Text('Texture pack folder (optional)')],[sg.In(),sg.FolderBrowse()],[sg.Button("Ok")]]
+branchselect = [[sg.Text("Paste github link to sm64pc repo and branch")],[sg.In(),sg.In(size=(7, 1))],[sg.Text("And type the name of repo folder")],[sg.In()],[sg.Text('modelpack folder (optional)')],[sg.In(),sg.FolderBrowse()],[sg.Text('Texture pack folder (optional)')],[sg.In(),sg.FolderBrowse()],[sg.Button("Ok")]]
 baserom = sg.FileBrowse()
 buildoptions = [[sg.Text('specify build flags and jobs, you can see possible flags on your repo\'s wiki, if you use modelpack, use MODELPACK=1, if you use texturepack, use EXTERNAL_DATA=1')],[sg.InputText(),sg.Button('Build')]]
 baseromselect = [[sg.Text("Select baserom of sm64 with extension .z64")],[
@@ -28,8 +28,8 @@ while True:
     # presses the OK button
     if event == "Ok" or event == sg.WIN_CLOSED:
         branchname=values[1]
-        os.system("git clone "+values[0]+' '+values[2]+' --branch='+values[1])
-        os.system('cp -r '+values[3]+'/actors '+values[2]+' && cp -r '+values[3]+'/src '+values[2])
+        os.system('git clone "'+values[0]+'" "'+values[2]+'" --branch='+values[1])
+        os.system('cp -r "'+values[3]+'/actors" "'+values[2]+'" && cp -r "'+values[3]+'/src" "'+values[2]+'"')
         repofolder=values[2]
         texturepack=values[4]
         window.close()
@@ -54,16 +54,16 @@ while True:
 
 
     os.system('cp '+'"'+baseromfolder+'"'+" "+'"'+baseromfolder+'_backup"')
-    os.system('mv "'+baseromfolder+'" '+repofolder+'/baserom.us.z64')
+    os.system('mv "'+baseromfolder+'_backup" "'+repofolder+'/baserom.us.z64"')
     print(buildflags)
-    os.system('bash -c "cd '+repofolder+' && make '+buildflags+'"')
-    os.system('cp -r '+texturepack+'/gfx '+repofolder+'/build/us_pc/res')
+    os.system('bash -c "cd "'+repofolder+'" && make '+buildflags+'"')
+    os.system('cp -r "'+texturepack+'/gfx" "'+repofolder+'/build/us_pc/res"')
     window = sg.Window('Build finished!', buildfinish)
     with open('builds.txt', 'r') as blist:
         builds = blist.read()
     with open ('builds.txt', 'w') as bwrite:
         bwrite.write(repofolder+'\n'+builds)
-    os.system('./'+repofolder+'/build/us_pc/sm64.us.f3dex2e')
+    os.system('./"'+repofolder+'/build/us_pc/sm64.us.f3dex2e"')
 
 
 
