f99b6fd0bf73363327d4fb107c1373f7b60e96f3
Filip <superpou1filip@gmail.com>
2021-02-09 14:38:34 +0100

    there's no way it doesnt work now.

--- installdepends.sh
+++ installdepends.sh
@@ -1,4 +1,6 @@
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 get-pip.py
-pip install tk bs4 pysimplegui
+sudo apt-get install python3-pip
+sudo pacman -S python-pip
+sudo yum install python3 python3-wheel
+sudo dnf install python3 python3-wheel
+sudo zypper install python3-pip python3-setuptools python3-wheel
 pip3 install tk bs4 pysimplegui
